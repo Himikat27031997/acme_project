ba097b90bf4280ef226d5f975aabcc1162221432
WorkB <muskullmorf@gmail.com>
2026-01-05 11:33:05 +0300

    Пытаем сделать явную дату в корретировке записи БД

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
