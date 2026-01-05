6d70fa80dea8b341c3287c04c5594b90bd39f222
Никита Казак <muskullmorf@gmail.com>
2026-01-05 23:24:14 +0300

    2

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 
 
 urlpatterns = [
     path('', include('pages.urls', namespace='pages')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls', namespace='birthday')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
